ed7f261ebfd05729623fcc8eda4754f889f62a52
ronnie <ronnie@guigu.cn>
2022-12-28 14:54:03 +0800

    second commit

--- src/main/java/com/itheima/test/GitTest.java
+++ src/main/java/com/itheima/test/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git...");
+        System.out.println("git2222");
     }
 }
