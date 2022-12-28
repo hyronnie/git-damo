3f9fdefc3d15521c4afe789ab7dd8a5758efbba1
ronnie <ronnie@guigu.cn>
2022-12-28 16:03:51 +0800

    hot-fix commit

--- src/main/java/com/itheima/test/GitTest.java
+++ src/main/java/com/itheima/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git...");
         System.out.println("git2222");
+        System.out.println("git333");
     }
 }
